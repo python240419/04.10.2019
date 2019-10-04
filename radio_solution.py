4f3240537e02d23f54b875f33e339a15d64f5534
python240419 <51232248+python240419@users.noreply.github.com>
2019-10-04 10:47:10 +0300

    Create radio_solution.py

---
+++ radio_solution.py
@@ -0,0 +1,36 @@
+from tkinter import *
+import time
+
+class CalculatorWindow:
+    def __init__(self, root):
+        self.root= root
+        root.title("Calculator")
+
+        MODES = [
+            ("Monochrome", "1"),
+            ("Grayscale", "L"),
+            ("True color", "RGB"),
+            ("Color separation", "CMYK"),
+        ]
+
+        v = IntVar()
+        v.set(" ")  # initialize
+
+        for n in range(1, 10):
+            b = Radiobutton(root, text=n,
+                            variable=v, value=n,
+                            indicatoron=0,
+                            width=30)
+            b.pack()
+        bn = Button(root, text="Print", command=lambda:print(v.get()))
+        bn.pack()
+
+root = Tk()
+my_window = CalculatorWindow(root)
+
+root.mainloop()  # blocking
+
+# targil:
+# create option buttons from 1-10 in loop (maybe without modes?)
+# print button -> will print the current selection
+
